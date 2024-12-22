76734ab64504c049a88f603a9d5c24b5ec5e5fca
Sanusi Hassan <sanusihassan122@gmail.com>
2024-12-22 20:07:57 +0200

    added base for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  base: isProd ? "/legal" : "/",
+
 });
